5bc9647af2e2d91f1592ef0a3d1baaeb876b1566
AnhKhoa585 <dinhducanhkhoa@gmail.com>
2025-03-27 08:57:24 +0700

    update visualizer.py

--- featureEngineering/Visualizer.py
+++ featureEngineering/Visualizer.py
@@ -1,17 +1,23 @@
 import pandas as pd
 import seaborn as sns
+import numpy as np
 import matplotlib.pyplot as plt
 import math
 import scipy.stats as stats
 
 class Visualizer:
     @staticmethod
-    def correlation_heatmap(df: pd.DataFrame, min_correlation: float, figsize=(10, 8), annot=True):
-        numeric_data = df.select_dtypes(include=['number'])
+    def correlation_heatmap(df, features_col, min_correlation, target = None, figsize=(20, 20), annot=True):
+        numeric_data = df[features_col]
         corr_matrix = numeric_data.corr()
 
         mask = abs(corr_matrix) < min_correlation
+
+        if(target is None):
+            target = features_col[0]
         filtered_corr = corr_matrix.mask(mask)
+        sort_order = filtered_corr[target].abs().sort_values(ascending=False).index
+        filtered_corr = filtered_corr.loc[sort_order, sort_order]
 
         plt.figure(figsize=figsize)
         sns.heatmap(filtered_corr, cmap="coolwarm", vmin=-1, vmax=1, annot=annot)
@@ -19,12 +25,12 @@ class Visualizer:
         plt.show()
         plt.close()
 
+
     @staticmethod
-    def target_feature_scatterplots(df: pd.DataFrame, target_col: str, cols_per_row=3, figsize=(5, 5)):
-        numeric_data = df.select_dtypes(include=['number'])
-        
-        if target_col not in numeric_data:
-            raise ValueError(f"'{target_col}' không phải là cột số hoặc không tồn tại trong DataFrame.")
+    def target_feature_scatterplots(df, features_col, target_col, cols_per_row=3, figsize=(5, 5)):
+        features_col.append(target_col)
+        features_col = list(set(features_col))
+        numeric_data = df[features_col]
         
         correlations = numeric_data.corr()[target_col]
         num_cols = [col for col in numeric_data.columns if col != target_col]
@@ -83,3 +89,52 @@ class Visualizer:
         plt.ylabel("sqrt(|Residuals|)")
         plt.show()
         plt.close()
+
+    @staticmethod
+    def boxplot(df, col, figsize=(5, 5)):
+        plt.figure(figsize=figsize)
+        sns.boxplot(x=df[col])
+        plt.title(f"Boxplot of {col}")
+        plt.show()
+        plt.close()
+    
+    @staticmethod
+    def histogram_plot(df, col, figsize=(5, 5)):
+        plt.figure(figsize=figsize)
+        sns.histplot(df[col], kde=True)
+        plt.title(f"Histogram of {col}")
+        plt.show()
+        plt.close()
+    
+    @staticmethod
+    def distribution_plot(df, numerical_features, figsize=(25,25)):
+        plt.figure(figsize=figsize)
+        for feature in numerical_features:
+            plt.subplot(5, 5, numerical_features.index(feature) + 1)
+            sns.histplot(data=df[feature], bins=20, kde=True)
+            plt.title(feature)
+        plt.tight_layout()
+        plt.show()
+    
+    def count_plot(df, categorical_features, figsize=(20, 10)):
+        num_plots = len(categorical_features)
+        ncols = 3
+        nrows = int(np.ceil(num_plots / ncols))
+        
+        fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=figsize)
+        axes = axes.flatten()
+        
+        for i, column in enumerate(categorical_features):
+            ax = axes[i]
+            sns.countplot(x=df[column], data=df, palette='bright', ax=ax, saturation=0.95)
+            for container in ax.containers:
+                ax.bar_label(container, color='black', size=10)
+            ax.set_title(f'Count Plot of {column.capitalize()}')
+            ax.set_xlabel(column.capitalize())
+            ax.set_ylabel('Count')
+        
+        for j in range(i+1, len(axes)):
+            fig.delaxes(axes[j])
+            
+        plt.tight_layout()
+        plt.show()
